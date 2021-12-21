514e614e98e40e0479c50b16cd1701683fa3f2f2
Thomas Sjerps <t.sjerps@student.tudelft.nl>
2021-12-21 14:21:12 +0100

    Move image files from res:// to user://

--- Code/Visualization/Main.gd
+++ Code/Visualization/Main.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
 
-export var topics: PoolStringArray
-
 var index := 0
 var count: int
 
@@ -13,6 +11,7 @@ var pos := Vector2()
 var calc_pitch := 0
 var pitch := 0.0
 
+
 onready var phaser_effect: AudioEffectPhaser = AudioServer.get_bus_effect(0, 2)
 onready var lowpass_effect: AudioEffectLowPassFilter = AudioServer.get_bus_effect(0, 4)
 
@@ -22,24 +21,39 @@ func _ready() -> void:
 	var covers := $Cover/Images
 	var content := $Content/Images
 	
+	# get topics from user files
+	var topics := []
+	var dir := Directory.new()
+	dir.open("user://images")
+	dir.list_dir_begin()
+
+	while true:
+		var next := dir.get_next()
+		if next == "":
+			break
+		
+		if not next.begins_with("."):
+			topics.push_back(next)
+	
+	dir.list_dir_end()
+	
 	assert(topics.size() != 0)
 	
 	count = topics.size()
 	
+	# create seamless loop
 	topics.push_back(topics[0])
 	
 	for i in range(topics.size()):
 		var topic: String = topics[i].to_lower()
 		
-		var cover_sprite := Sprite.new()
-		cover_sprite.texture = load("images/" + topic + "/cover.png")
+		var cover_sprite := _sprite_from_path("user://images/" + topic + "/cover.png")
 		covers.add_child(cover_sprite)
 		cover_sprite.name = topic.capitalize()
 		cover_sprite.position = Vector2(1200 * i, 0)
 		cover_sprite.centered = false
 		
-		var content_sprite := Sprite.new()
-		content_sprite.texture = load("images/" + topic + "/content.png")
+		var content_sprite := _sprite_from_path("user://images/" + topic + "/content.png")
 		content.add_child(content_sprite)
 		cover_sprite.name = topic.capitalize()
 		content_sprite.position = Vector2(1200 * i, 0)
@@ -95,6 +109,21 @@ func go_right() -> void:
 	reset_squares()
 
 
+# resets all squares
 func reset_squares() -> void:
 	for square in $MainViewport/Viewport/Grid.get_children():
 		square.reset()
+
+
+# creates a sprite from an image path
+func _sprite_from_path(path: String) -> Sprite:
+	var image := Image.new()
+	image.load(path)
+	
+	var texture := ImageTexture.new()
+	texture.create_from_image(image)
+	
+	var sprite := Sprite.new()
+	sprite.texture = texture
+	
+	return sprite
